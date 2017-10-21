50b88a9d379903ffbd8b4b1487bf541bffd7ef66
Kubilay Karpat <karpatkubilay@gmail.com>
2017-10-22 00:46:23 +0300

    Missing init.py entries added & issue fetch added to stat collector

--- github_trends/gihub_parser/stat_collecter.py
+++ github_trends/gihub_parser/stat_collecter.py
@@ -1,18 +1,22 @@
 from github_trends.gihub_parser.commit_fetcher import CommitFetcher
+from github_trends.gihub_parser.issue_fetcher import IssueFetcher
 from github_trends.gihub_parser.star_and_fork_fetcher import StarAndForkFetcher
 
 
 class StatCollecter:
     def __init__(self):
         self.commit_fetcher = CommitFetcher()
+        self.issue_fetcher = IssueFetcher()
         self.star_and_fork_fetcher = StarAndForkFetcher()
 
     def collect_repo_stats(self, repo_list):
         for repo in repo_list:
             self.commit_fetcher.fetch_and_save_commits_of_repo(repo["owner"], repo["name"])
+            self.issue_fetcher.fetch_and_save_commits_of_repo(repo["owner"], repo["name"])
             self.star_and_fork_fetcher.fetch_and_save_stars_of_repo(repo["owner"], repo["name"])
             self.star_and_fork_fetcher.fetch_and_save_forks_of_repo(repo["owner"], repo["name"])
         return
+
 
 if __name__ == '__main__':
     sc = StatCollecter()
