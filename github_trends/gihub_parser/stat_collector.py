811b3f2f26917f0c61d94e9af427a4627f7f1c5a
Kubilay Karpat <kubilay@snapbytes.com>
2018-06-21 13:56:18 +0300

    Stakoverflow fetcher removed

--- github_trends/gihub_parser/stat_collector.py
+++ github_trends/gihub_parser/stat_collector.py
@@ -2,7 +2,6 @@ from github_trends import category_repos
 from github_trends.gihub_parser.commit_fetcher import CommitFetcher
 from github_trends.gihub_parser.issue_fetcher import IssueFetcher
 from github_trends.gihub_parser.release_fetcher import ReleaseFetcher
-from github_trends.gihub_parser.stackoverflow_fetcher import StackOverflowFetcher
 from github_trends.gihub_parser.star_and_fork_fetcher import StarAndForkFetcher
 from github_trends.services.database_service import DatabaseService
 
@@ -15,16 +14,13 @@ class StatCollector:
         self.issue_fetcher = IssueFetcher()
         self.star_and_fork_fetcher = StarAndForkFetcher()
         self.release_fetcher = ReleaseFetcher()
-        self.stack_overflow_fetcher = StackOverflowFetcher()
 
     def collect_repo_stats(self, repo_list):
         for repo in repo_list:
             self.commit_fetcher.fetch_and_save_commits_of_repo(repo["owner"], repo["name"])
             self.issue_fetcher.fetch_and_save_issues_of_repo(repo["owner"], repo["name"])
             self.star_and_fork_fetcher.fetch_and_save_stars_of_repo(repo["owner"], repo["name"])
-            self.star_and_fork_fetcher.fetch_and_save_forks_of_repo(repo["owner"], repo["name"])
             self.release_fetcher.fetch_and_save_releases_of_repo(repo["owner"], repo["name"])
-        self.stack_overflow_fetcher.fetch_and_save_so_stats_of_repo(repo_list)
 
 
 if __name__ == '__main__':
